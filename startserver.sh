3ac139d1cc394a37b799ea378f87c5f5bd305e1c
L4ma <marco@mail.marcos-it.de>
2019-11-08 08:23:56 +0100

    New Feature with a question

--- startserver.sh
+++ startserver.sh
@@ -1,4 +1,10 @@
 clear
-echo -e "Starting Minecraft Server in 3 seconds.."
-sleep 3
-screen -A -m -d -S minecraftserver java -Xmx2048M -Xms2048M -jar minecraft_server.jar nogui
+read -p  "Dou you want so start the server? (Yes/No)" starting
+if [ $starting == yes && Yes]
+	then
+	echo -e "Starting Minecraft Server in 3 seconds.."
+	sleep 3
+	screen -A -m -d -S minecraftserver java -Xmx2048M -Xms2048M -jar minecraft_server.jar nogui
+	else
+	echo "Okay, bye :)"
+fi
